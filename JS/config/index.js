7ab316250f2e0736cea28b705dec76ff54dbcb5c
WilliamEduardo20 <williameduardominorelli@gmail.com>
2024-10-26 22:26:07 -0300

    22:25 26/10/2024
    Tela de Configurações criada.

--- JS/config/index.js
+++ JS/config/index.js
@@ -4,3 +4,17 @@ import { mudaModo } from "../Funcionalidades/modo/escuro.js";
 navLateral();
 
 mudaModo();
+
+/Seleciona o item e add Fundo/
+const menuItem = document.querySelectorAll(".sub-pagina");
+
+function selecLink() {
+    menuItem.forEach((item) => 
+        item.classList.remove("ativado")
+    );
+    this.classList.add("ativado");
+}
+
+menuItem.forEach((item) => 
+    item.addEventListener('click', selecLink)
+)
